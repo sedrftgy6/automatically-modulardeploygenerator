bf893f4b5fc28c65f8588d2b7140e3e44601b703
sedrftgy6 <js1sabraao1@hotmail.com>
2024-05-07 05:52:20 +0700

    aptos

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const gnomeSort = (arr) => {
-  let index = 0;
-  while (index < arr.length) {
-    if (index === 0 || arr[index] >= arr[index - 1]) {
-      index++;
-    } else {
-      [arr[index], arr[index - 1]] = [arr[index - 1], arr[index]];
-      index--;
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
   }
   return arr;
-};
+}

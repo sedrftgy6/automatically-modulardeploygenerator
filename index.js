7084eedce6ff56df30aea13d38ae7e41c439072a
sedrftgy6 <js1sabraao1@hotmail.com>
2024-05-08 01:33:35 +0700

    start

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+function canPartitionKSubsets(nums, k) {
+  const sum = nums.reduce((acc, val) => acc + val, 0);
+  if (sum % k !== 0) return false;
+  const target = sum / k;
+  nums.sort((a, b) => b - a);
+  if (nums[0] > target) return false;
+  const visited = new Array(nums.length).fill(false);
+  return backtrack(0, 0, k);
+  function backtrack(start, currentSum, groups) {
+    if (groups === 1) return true;
+    if (currentSum === target) return backtrack(0, 0, groups - 1);
+    for (let i = start; i < nums.length; i++) {
+      if (!visited[i] && currentSum + nums[i] <= target) {
+        visited[i] = true;
+        if (backtrack(i + 1, currentSum + nums[i], groups)) return true;
+        visited[i] = false;
       }
     }
+    return false;
   }
-  return arr;
 }
